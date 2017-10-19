071bb408c5bfdde964e32215406c2602609fa150
wribbe <eng_steff@hotmail.com>
2017-10-19 12:39:52 +0200

    Update coverage table.

--- proj5/create_table.py
+++ proj5/create_table.py
@@ -9,15 +9,15 @@ if not os.path.isdir(DIR_OUTPUT):
     os.mkdir(DIR_OUTPUT)
 
 
-problems = ["P{}".format(i) for i in range(13)]
-solutions = ["S{}".format(i) for i in range(13)]
+problems = ["\\rotatebox{{90}}{{P{}.some-name}}".format(i) for i in range(14)]
+solutions = ["S{}.some-name".format(i) for i in range(14)]
 
 base_table = """
-\\begin{{tabular}}{{{}}}
- \\hline
-  {}
- \\hline
-{}
+\\begin{{tabular}}{{{0}}}
+ \\cline{{{1}-{2}}}
+ \\multicolumn{{1}}{{c|}}{{}}{3}
+ \\cline{{{1}-{2}}}
+{4}
 \\end{{tabular}}
 """
 
@@ -28,11 +28,11 @@ def make_table_row(item_list):
     item_list += "\\hline"
     return item_list
 
-option_tabular = ("| r |"+"| c "*len(problems))+'|'
+option_tabular = ("| r "+"| c "*len(problems))+'|'
 heading = make_table_row([' ']+problems)
-content = '\n'.join(["S{} & ".format(i)+make_table_row(' '*len(problems)) for
-    i, row in enumerate(solutions)])
+content = '\n'.join(["{} & ".format(name)+\
+        make_table_row(' '*len(problems)) for name in solutions])
 
 with open(PATH_OUTPUT, 'w') as fp:
-    fp.write(base_table.format(option_tabular, heading, content))
+    fp.write(base_table.format(option_tabular, 2, 15, heading, content))
     fp.write('\n')
