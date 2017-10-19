212c69afa487a98cfce6b2d1df9b6b06fe5f9579
wribbe <eng_steff@hotmail.com>
2017-10-19 21:34:40 +0200

    Whitespace.

--- proj5/generate_rational_matrix.py
+++ proj5/generate_rational_matrix.py
@@ -9,18 +9,18 @@ table = [
             position or opened, the tamper sensors will detect this and notify
             maintenance personnel.
             """),
-        ("t.network", "o.trustzone_nx o.secure_comms o.two_ways_prot", 
+        ("t.network", "o.trustzone_nx o.secure_comms o.two_ways_prot",
         	"""
         	Explain why
         	"""),
-        ("t.mismanage", "o.no_tamper \no.id a.no_adversarial", 
+        ("t.mismanage", "o.no_tamper \no.id a.no_adversarial",
         	"""
         	Tamper detection will trigger if camera is not mounted correctly
         	or case is not closed correctly when mounting or remounting. ID will help how?
         	The background checks that are carried out on personnel are partly done to catch
         	any potential criminal record but also to screen for persons that can perform well in their job.
         	"""),
-        ("t.adversarial", "o.pwr_out o.attest o.trustzone_nx o.two_ways_prot a.no_adversarial", 
+        ("t.adversarial", "o.pwr_out o.attest o.trustzone_nx o.two_ways_prot a.no_adversarial",
         	"""
         	Attestation is carried out in order
         	to check that the right firmware is loaded and that relevant PCRs (Platform Configuration Registers) in the TPM have
@@ -28,28 +28,28 @@ table = [
         	to inject code. Lastly, here as well the assumption that background checks are carried out on personnel will in part
         	mitigate this threat.
         	"""),
-        ("t.persistent", "o.pwr_out o.two_way_prot", 
+        ("t.persistent", "o.pwr_out o.two_way_prot",
         	"""
         	The two-way protection makes it difficult to intercept SRTP communications.
         	"""),
-        ("t.lost_asset", "o.no_tamper o.enc_data o.secure_comms", 
+        ("t.lost_asset", "o.no_tamper o.enc_data o.secure_comms",
         	"""
         	The tamper detection will detect if camera is removed from wall and/or
         	opened. The secure communications objective means user and video data are hard to steal. If an attacker removes flash
         	memory the user data and video stored there are encrypted.
         	"""),
-        ("t.mng_test", "o.trustzone_nx o.decomm", 
+        ("t.mng_test", "o.trustzone_nx o.decomm",
         	"""
         	"""),
-        ("t.signed_fw", "o.tpm_key_strg o.decomm o.no_tamper o.id", 
+        ("t.signed_fw", "o.tpm_key_strg o.decomm o.no_tamper o.id",
         	"""
         	Keys are stored in the camera's TPM
         	so an attacker can not first have physical access to camera and get hold of keys and later perform remote code injection.
         	Tamper detection will make it harder to undetected access camera's internal parts.
         	"""),
-        ("t.srtp_recv", "o.no_tamper o.two_ways_prot", 
+        ("t.srtp_recv", "o.no_tamper o.two_ways_prot",
         	"""
-        	Tamper detection means that an attacker which tries to access internal parts of camera have a higher probability 
+        	Tamper detection means that an attacker which tries to access internal parts of camera have a higher probability
         	of being detected. If the key derivation function is reapplied, it will be discovered that a key has changed.
         	"""),
         ("t.flash_intg", "o.tpm_seal o.enc_data o.id",
@@ -60,9 +60,9 @@ table = [
                 produces other hashes will prompt the platform to stop and
                 signal that something is wrong.
         	"""),
-        ("t.jtag_abuse", "o.trustzone_nx o.tpm_key_strg o.secure_comms", 
+        ("t.jtag_abuse", "o.trustzone_nx o.tpm_key_strg o.secure_comms",
         	"""
-        	Write xor execute protection means that even if an attacker can abuse JTAG interface, no executable harmful code 
+        	Write xor execute protection means that even if an attacker can abuse JTAG interface, no executable harmful code
         	can be loaded. An attacker also needs the keys that are stored in the TPM.
         	"""),
        ]
