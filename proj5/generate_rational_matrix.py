d49e58b6c20ce1fb4e3188cc580edd34bbcdb27b
Rasmus Olofzon <rasmus.olofzon@gmail.com>
2017-10-19 23:01:25 +0200

    tables almost finished

--- proj5/generate_rational_matrix.py
+++ proj5/generate_rational_matrix.py
@@ -1,7 +1,7 @@
 import os
 
 table = [
-        ("t.physical", "o.tamper a.location ",
+        ("t.physical", "o.tamper o.pwr_out a.location ",
             """
             Since the camera is assumed to be located at a position which is
             not reachable unassisted, unauthorized direct physical access to the unit should
@@ -9,19 +9,19 @@ table = [
             position or opened, the tamper sensors will detect this and notify
             maintenance personnel.
             """),
-        ("t.network", "o.trustzone_nx o.secure_comms o.two_ways_prot",
+        ("t.network", "o.secure_comms o.two_ways_prot",
         	"""
         	A correctly configured SRTP protocol with forward and backward protection should provide protection for 4G attacks
         	(e. g. false base stations) while still keeping the product at a lower price point.
         	"""),
-        ("t.mismanage", "o.no_tamper \no.id a.no_adversarial",
+        ("t.mismanage", "o.no_tamper o.tpm_seal o.id a.no_adversarial",
         	"""
         	Tamper detection will trigger if camera is not mounted correctly
         	or case is not closed correctly when mounting or remounting. ID will help how?
         	The background checks that are carried out on personnel are partly done to catch
         	any potential criminal record but also to screen for persons that can perform well in their job.
         	"""),
-        ("t.adversarial", "o.pwr_out o.attest o.trustzone_nx o.two_ways_prot a.no_adversarial",
+        ("t.adversarial", "o.attest o.trustzone_nx o.two_ways_prot o.tpm_seal a.no_adversarial",
         	"""
         	Attestation is carried out in order
         	to check that the right firmware is loaded and that relevant PCRs (Platform Configuration Registers) in the TPM have
@@ -31,20 +31,21 @@ table = [
         	Lastly, here as well the assumption that background checks are carried out on personnel will in part
         	mitigate this threat.
         	"""),
-        ("t.persistent", "o.pwr_out o.two_way_prot",
+        ("t.persistent", "o.two_way_prot",
         	"""
         	The two-way protection makes it difficult to intercept SRTP communications.
         	"""),
         ("t.lost_asset", "o.no_tamper o.enc_data o.secure_comms",
         	"""
         	The tamper detection will detect if camera is removed from wall and/or
-        	opened. The secure communications objective means user and video data are hard to steal. If an attacker removes flash
-        	memory the user data and video stored there are encrypted.
+        	opened. The secure communications objective means user and video data are hard to steal in-transit. 
+        	If an attacker removes flash memory the user data and video stored there are encrypted.
         	"""),
-        ("t.mng_test", "o.trustzone_nx o.decomm",
+        ("t.mng_test", "o.trustzone_nx",
         	"""
+
         	"""),
-        ("t.signed_fw", "o.tpm_key_strg o.decomm o.no_tamper o.id",
+        ("t.signed_fw", "o.tpm_key_strg o.no_tamper o.id",
         	"""
         	Keys are stored in the camera's TPM
         	so an attacker can not first have physical access to camera and get hold of keys and later perform remote code injection.
@@ -55,7 +56,7 @@ table = [
         	Tamper detection means that an attacker which tries to access internal parts of camera have a higher probability
         	of being detected. If the key derivation function is reapplied, it will be discovered that a key has changed.
         	"""),
-        ("t.flash_intg", "o.tpm_seal o.enc_data o.id",
+        ("t.flash_intg", "o.tpm_seal o.enc_data",
         	"""
                 By using the TPMs seal functionality vital system resources
                 can be tied to hashes of the correct configuration data on the
@@ -63,10 +64,10 @@ table = [
                 produces other hashes will prompt the platform to stop and
                 signal that something is wrong.
         	"""),
-        ("t.jtag_abuse", "o.trustzone_nx o.tpm_key_strg o.secure_comms",
+        ("t.jtag_abuse", "o.trustzone_nx a.no_adversarial",
         	"""
         	Write xor execute protection means that even if an attacker can abuse JTAG interface, no executable harmful code
-        	can be loaded. An attacker also needs the keys that are stored in the TPM.
+        	can be loaded. The assumption of background checks on personnel holds here too.
         	"""),
        ]
 
