0d37060669a7620890cc9cef93c26a884619bbff
Rasmus Olofzon <rasmus.olofzon@gmail.com>
2017-10-24 12:47:53 +0200

    added others' report 5 for peer review, also made skeleton documents for reviewing

---
+++ proj5/generate_peer_review_matrix2.py
@@ -0,0 +1,30 @@
+import os
+
+table = [
+        ("A Report structure", "2", ""),
+        ("B Product description + design requirements", "3", ""),
+        ("C Assumptions", "3", ""),
+        ("\\textbf{D High-level architectural overview}", "", ""),
+        ("D1 General", "4", ""),
+        ("D2 TOE and ST", "5", ""),
+        ("\\textbf{E Security evaluation of design + summary}", "", ""),
+        ("E1 General", "5", ""),
+        ("E2 Evaluation", "4", ""),
+       ]
+
+row_format = "\\parbox{{6cm}}{{\\vspace{{3.0pt}} {} }} & {} & {} \\\\"
+content = []
+
+content = content
+for desc, maxp, deduction in table:
+    content.append(row_format.format(desc, maxp, deduction))
+    content.append("\\hline")
+
+content.append("\\hline")
+content.append(row_format.format("Total:", "26", "X"))
+content.append("\\hline")
+content.append(row_format.format("Score:", "8", "X/3"))
+content.append("\\hline")
+
+with open(os.path.join('input','peer2.tex'), 'w') as fp:
+   fp.write('\n'.join(content)+'\n')
